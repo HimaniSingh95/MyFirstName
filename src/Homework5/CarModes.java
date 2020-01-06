aaada48f6fe85dacca3968a1d5b460c435c162bc
Himani Singh <singhhimani4995@gmail.com>
2020-01-05 23:09:44 -0500

    homework

--- src/Homework5/CarModes.java
+++ src/Homework5/CarModes.java
@@ -1,12 +1,34 @@
 package Homework5;
 
+import java.util.Scanner;
+
 public class CarModes {
     public static void main(String args[]){
-        String CarMode="D";
+
+        Scanner Mode=new Scanner(System.in);
+        System.out.println("Enter car mode : ");
+        String CarMode=Mode.next();
+
         switch(CarMode){
             case "P" :
-                System.out.println();
+            case "Parallel":
+                System.out.println("Park between two cars.");
+                break;
 
+            case "D":
+                System.out.println("You can put drive tpe to Snow type, Sport type or Regular type");
+                break;
+
+            case "N":
+                System.out.println("You can put car in car wash station.");
+                break;
+
+            case "R":
+                System.out.println("You can only reverse the car and car will activate the back camera.");
+                break;
+
+            default:
+                System.out.println("Invalid mode type, Please check your car mode.");
         }
     }
 }
